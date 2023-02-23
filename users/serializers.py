5c0df2b975a68fa6ddbfe430eaf47cbfffca3c33
Luiza Barros <thecoderana@gmail.com>
2023-02-23 14:23:33 -0300

    feat: task 1 incomplete

--- users/serializers.py
+++ users/serializers.py
@@ -6,21 +6,23 @@ from .models import User
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     username = serializers.CharField(max_length=127)
-    email = serializers.EmailField(max_length=127, unique=True)
+    email = serializers.EmailField(max_length=127)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
-    birthdate = serializers.DateField(null=True)
-    is_employee = serializers.BooleanField(null=True, default=False)
+    birthdate = serializers.DateField(allow_null=True, default=None)
+    is_employee = serializers.BooleanField(default=False)
     password = serializers.CharField(write_only=True)
     is_superuser = serializers.BooleanField(read_only=True)
 
     validators = [
         UniqueTogetherValidator(
-            queryset=User.objects.all(), fields=["email", "username"]
+            queryset=User.objects.all(),
+            fields=["username", "email"],
         )
     ]
 
     def create(self, validated_data):
-        if validated_data.is_employee:
+        if validated_data["is_employee"]:
             return User.objects.create_superuser(**validated_data)
-        return User.objects.create_user(**validated_data)
+        else:
+            return User.objects.create_user(**validated_data)
